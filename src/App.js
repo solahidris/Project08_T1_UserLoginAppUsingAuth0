b12529c31d14eca0032803ade808179c2482e563
solahidris <emailsolah@gmail.com>
2023-05-08 23:35:47 +0800

    added app description and tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import AppDescription from "./AppDescription";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className='font-bold'>so this is Tailwind huh</p>
+        <p className="font-bold">so this is Tailwind huh</p>
+        <AppDescription />
       </header>
     </div>
   );
---
+++ src/AppDescription.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const AppDescription = () => {
+  return (
+    <div>
+      <p className="text-xs px-8 py-3 font-mono">
+        Create a simple authentication app where
+      </p>
+      <p className="text-xs px-8 py-0 font-mono">
+        users can log in using Auth0 and
+      </p>
+      <p className="text-xs px-8 py-0 font-mono">view the user data</p>
+      <p className="text-xs px-8 py-0 font-mono">with an option to log out</p>
+      <p> ---------------------------------- </p>
+      <p className="text-xs px-8 py-1 font-mono font-bold">steps breakdown</p>
+      <p className="text-xs text-left px-8 py-0 font-mono">0- check what is auth0</p>
+      <p className="text-xs text-left px-8 py-0 font-mono">1- create home page </p>
+      <p className="text-xs text-left px-8 py-0 font-mono">2- c logged in page </p>
+      <p className="text-xs text-left px-8 py-0 font-mono">3- c login form username & password </p>
+      <p className="text-xs text-left px-8 py-0 font-mono">4- c login submit button</p>
+      <p className="text-xs text-left px-8 py-0 font-mono">5- c logout button</p>
+      <p className="text-xs text-left px-8 py-0 font-mono">6- c routes?</p>
+    </div>
+  );
+};
+
+export default AppDescription;
